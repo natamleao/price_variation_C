eda6efecc01c442d2daa7310f3778e22ec8a7a2b
Natam Ferreira <natam@kali.kali.edu>
2022-07-21 16:04:52 -0300

    Código atualizado, está todo estruturado com os diretórios

---
+++ apps/app.c
@@ -0,0 +1,23 @@
+#include "easterEggs.h"
+#include <stdio.h>
+
+int main(){
+
+    List *ListEggs = ListCreate();
+    int numberEggs, identifier;
+    float price;
+
+    printf("\n+----------------------------- ADICIONAR NA LISTA -----------------------------+\n\n");
+    printf("INFORME A QUANTIDADE DE OVOS: ");
+    scanf("%d", &numberEggs);
+    puts("");
+    for(int i = 0; i < numberEggs; i++){
+        printf("INFORME O IDENTIFICADOR E O PREÇO DO %dº OVO: ", (i+1));
+        scanf(" %d%f", &identifier, &price);
+        ListAdd(ListEggs, identifier, price);
+    }
+
+    EggsSearchPrice(ListEggs);
+
+    return 0;
+}
